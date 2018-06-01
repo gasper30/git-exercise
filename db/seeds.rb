bce9976cdabde093149bcf93f80cef7bde8c2909
gasper <bricktowntw@gmail.com>
2018-06-01 10:06:16 +0800

    update README.md

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times do |i|
-  user = user.create!(name: "user_#{i+1}")
+  user = User.create!(name: "user#{i+1}")
   puts "#{user.name} created"
 end
